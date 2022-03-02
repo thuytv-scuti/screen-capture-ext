d3da46cf4c53f25cba5230ee6d7f24ac84811380
thuytv <thuytv@scuti.asia>
2022-03-02 11:18:31 +0700

    update popup position

--- src/pages/Background/index.js
+++ src/pages/Background/index.js
@@ -1,16 +1,27 @@
 import { sendScreenshotToBoard } from '../../utils';
+
+const calculateCenterScreen = async (width, height) => {
+  const _window = await chrome.windows.getCurrent();
+  const top = Math.max(150, Math.round((_window.height - height) / 2));
+	const left = Math.max(150, Math.round((_window.width - width) / 2));
+
+  return { top, left };
+}
+
 chrome.action.onClicked.addListener(async (_) => {
-  const boards = await chrome.tabs.query({ url: '*://*/*/board-digital/*' });
+  const boards = await chrome.tabs.query({ url: '*://*/*/board-digital/*', status: 'complete' });
+  console.log('[x] current window', await chrome.windows.getCurrent());
 
   // no board opening
   if (boards.length === 0) {
+    const pos = await calculateCenterScreen(500, 250);
     return chrome.windows.create({
       type: 'popup',
       focused: true,
       width: 500,
       height: 250,
-      top: 150,
-      left: 150,
+      top: pos.top,
+      left: pos.left,
       url: `popup.html?isEmpty=1`
     });
   }
@@ -24,17 +35,19 @@ chrome.action.onClicked.addListener(async (_) => {
         format: 'png',
       }, resolve)
     );
+
     return sendScreenshotToBoard(boards[0], screenshot, currentTab.url);
   }
 
+  const pos = await calculateCenterScreen(600, 600);
   // multiple boards are opening
   chrome.windows.create({
     type: 'popup',
     focused: true,
     width: 600,
     height: 600,
-    top: 150,
-    left: 150,
+    top: pos.top,
+    left: pos.left,
     url: `popup.html?windowId=${currentTab.windowId}&tabId=${currentTab.id}`
   });
 });
--- src/utils.js
+++ src/utils.js
@@ -1,18 +1,28 @@
-export const sendScreenshotToBoard = (board, image, url) => {
-  const dispatchImageCaptured = (_image, _url) => {
-    const insertCaptureEvent = new CustomEvent('insert:captured', {
-      detail: {
-        image: _image,
-        url: _url,
-      },
-    });
+const _dispatchImageCaptured = (_image, _url) => {
+  const insertCaptureEvent = new CustomEvent('insert:captured', {
+    detail: {
+      image: _image,
+      url: _url,
+    },
+  });
 
-    window.dispatchEvent(insertCaptureEvent);
-  };
+  window.dispatchEvent(insertCaptureEvent);
+};
 
+const _sendScreenshotToBoard = (board, image, url) => {
   chrome.scripting.executeScript({
     target: { tabId: board.id },
-    func: dispatchImageCaptured,
+    func: _dispatchImageCaptured,
     args: [image, url],
   });
 };
+
+export const sendScreenshotToBoard = (board, image, url) => {
+  try {
+    return _sendScreenshotToBoard(board, image, url)
+  } catch(err) {
+    console.log('[x] ERROR:', err);
+  }
+  
+  return null;
+}
